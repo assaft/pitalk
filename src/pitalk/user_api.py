300497fc69172d8058a0995a244d83e616c2a20d
assaft <assaftl.net@gmail.com>
2024-12-31 12:04:25 +0200

    updates

--- src/pitalk/user_api.py
+++ src/pitalk/user_api.py
@@ -22,7 +22,7 @@ class FriendCard(BaseModel):
 
 class User:
 
-    PITALK_USER_PATH = Path("~/.pitalk/")
+    PITALK_USER_PATH = Path("/home/.pitalk/")
 
     def __init__(self, user_name: str | None = None):
         if not user_name:
